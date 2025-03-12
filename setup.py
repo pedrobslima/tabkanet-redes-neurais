f515858e1b0d2bd8775f806caec9350773db7a78
Pedro Lima <102550244+pedrobslima@users.noreply.github.com>
2025-03-12 10:57:18 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# Load the long_description from README.md
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TabKANet",
+    version="0.2",
+    description="Mesclagem de KAN e Transformers para melhor modelagem de Dados Tabulares",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/kindxiaoming/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    'torch>=2.3',
+    'pandas>=2.0'
+  ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
